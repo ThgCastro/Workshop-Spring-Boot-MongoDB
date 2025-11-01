1ad6f7e9ca376044710a784cd93452246276adb0
Thiago Castro <nascimentocastro989@gmail.com>
2025-11-01 12:36:41 -0300

    Obtendo um usuario por id com exceções

--- src/main/java/com/thgcastro/workshopmongo/services/UserService.java
+++ src/main/java/com/thgcastro/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.thgcastro.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thgcastro.workshopmongo.domain.User;
 import com.thgcastro.workshopmongo.repositoriy.UserRepository;
+import com.thgcastro.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
 	}
 }
