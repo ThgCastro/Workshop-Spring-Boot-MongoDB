993687f128c6136562c822b6ab018dff6f071beb
Thiago Castro <nascimentocastro989@gmail.com>
2025-11-03 09:45:09 -0300

    Obtendo um post por id

---
+++ src/main/java/com/thgcastro/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.thgcastro.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thgcastro.workshopmongo.domain.Post;
+import com.thgcastro.workshopmongo.repositoriy.PostRepository;
+import com.thgcastro.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));	
+	}
+
+}
